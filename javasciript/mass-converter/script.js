f71aeaa4a403c0c48949d4706c54eebca503fb93
aasith <aasith32@gmail.com>
2026-04-20 12:53:54 +0530

    mass-converter html,css,js files are added

---
+++ javasciript/mass-converter/script.js
@@ -0,0 +1,11 @@
+function converter(){
+     let value = document.getElementById("user input"). value;
+     let firstSelect = document.getElementById("firstSelect"). value;
+     let  secondSelect = document.getElementById("secondSelect"). value;
+     let  Display = document.getElementById("Display");
+}
+
+
+if(value){
+    if()
+}
