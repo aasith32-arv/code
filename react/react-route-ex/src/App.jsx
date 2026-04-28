fd33edf4f92b3654c18d019e29e002e7c711deab
aasith <aasith32@gmail.com>
2026-04-28 14:23:23 +0530

    home page are added

--- react/react-route-ex/src/App.jsx
+++ react/react-route-ex/src/App.jsx
@@ -4,17 +4,23 @@ import Contact from './pages/Contact'
 import About from './pages/About'
 import Footer from './components/Footer'
 import Header from './components/Header'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   
   return (
     <>
      <h1>React-rout-ex</h1>
+
      <Header/>
      <Contact/>
      <About/>
      <Footer/>
-     
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={}>
+    </Routes>
+    </BrowserRouter>
     </>
   )
 }
